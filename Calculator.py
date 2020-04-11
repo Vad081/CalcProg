1dbf11087e9d3c56eae1c0a5f6d22cc2f409c59b
Vadim Ayupov <aupovv@gmail.com>
2020-04-11 09:49:49 +0300

    beta 2.1

--- Calculator.py
+++ Calculator.py
@@ -3,19 +3,21 @@ from CalcMain import Ui_Calculator
 
 from math import pi,sqrt
 from random import uniform
+import string
 
 
 class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
 
-    firstNum = None
-    userIsTypingSecondNumber = False
+    # firstNum = None
+    # userIsTypingSecondNumber = False # нужно сделать закрытыми
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
+        self._firstNum = None
+        self._userIsTypingSecondNumber = False
 
-        # fixme: сделать нажатие а не клик
         self.pushButton_0.clicked.connect(self.digitClicked)
         self.pushButton_1.clicked.connect(self.digitClicked)
         self.pushButton_2.clicked.connect(self.digitClicked)
@@ -27,15 +29,6 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.pushButton_8.clicked.connect(self.digitClicked)
         self.pushButton_9.clicked.connect(self.digitClicked)
 
-        # self.pushButton_A.clicked.connect(self.letterClicked)
-        # self.pushButton_B.clicked.connect(self.letterClicked)
-        # self.pushButton_C.clicked.connect(self.letterClicked)
-        # self.pushButton_D.clicked.connect(self.letterClicked)
-        # self.pushButton_E.clicked.connect(self.letterClicked)
-        # self.pushButton_F.clicked.connect(self.letterClicked)
-
-
-
         self.pushButton_decimal.clicked.connect(self.decimalClicked)
         self.pushButton_unaryMinus.clicked.connect(self.unaryOperationsClicked)
         self.pushButton_percent.clicked.connect(self.unaryOperationsClicked)
@@ -44,6 +37,7 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.pushButton_minus.clicked.connect(self.binaryOperationsClicked)
         self.pushButton_multiply.clicked.connect(self.binaryOperationsClicked)
         self.pushButton_divide.clicked.connect(self.binaryOperationsClicked)
+
         self.pushButton_equals.clicked.connect(self.equalsClicked)
         self.pushButton_allClear.clicked.connect(self.clearClicked)
 
@@ -57,20 +51,11 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
 
         self.pushButton_convert.clicked.connect(self.convertGetResulst)
 
-        # self.pushButton_bin.clicked.connect(self.binSystem)
-
-
         self.pushButton_plus.setCheckable(True)
         self.pushButton_minus.setCheckable(True)
         self.pushButton_multiply.setCheckable(True)
         self.pushButton_divide.setCheckable(True)
 
-        # self.pushButton_A.setCheckable(True)
-        # self.pushButton_B.setCheckable(True)
-        # self.pushButton_C.setCheckable(True)
-        # self.pushButton_D.setCheckable(True)
-        # self.pushButton_E.setCheckable(True)
-        # self.pushButton_F.setCheckable(True)
 
         self.pushButton_bin.setCheckable(True)
         self.pushButton_oct.setCheckable(True)
@@ -78,70 +63,64 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
 
 
 
+    def outputLabel(self,labelNumber):
+        '''Функция для ввода нового значения на экран'''
+        if isinstance(labelNumber,float):   
+        	newLabel = format(labelNumber, '.15g')
+        elif isinstance(labelNumber,str):
+        	newLabel = format(labelNumber, '.15s')
+        
+        return self.label.setText(newLabel)
+
+
     def digitClicked(self):
+        '''Введены цифры'''
+        
         button = self.sender()
 
         if ((self.pushButton_plus.isChecked() or 
             self.pushButton_minus.isChecked() or 
             self.pushButton_multiply.isChecked() or 
-            self.pushButton_divide.isChecked()) and (not self.userIsTypingSecondNumber)):
+            self.pushButton_divide.isChecked()) and (not self._userIsTypingSecondNumber)):
             newLabel = format(float(button.text()), '.15g')
-            self.userIsTypingSecondNumber = True
+            self._userIsTypingSecondNumber = True
         elif (('.' in self.label.text()) and button.text() == '0'):
             newLabel = format(self.label.text() + button.text(),'.15')
+        elif not self.label.text().isdigit():
+            newLabel = format(button.text(), '.15')
         else:
             newLabel = format(float(self.label.text() + button.text()), '.15g')
 
-        self.label.setText(newLabel)
-        
-
-
-    # def letterClicked(self):
-    #     button = self.sender()
-
-    #     if ((self.pushButton_A.isChecked() or 
-    #         self.pushButton_B.isChecked() or 
-    #         self.pushButton_C.isChecked() or 
-    #         self.pushButton_D.isChecked() or
-    #         self.pushButton_E.isChecked() or
-    #         self.pushButton_F.isChecked()) and (not self.userIsTypingSecondNumber)):
-    #         newLabel = format(str(button.text()), '.15s')
-    #         self.userIsTypingSecondNumber = True
-    #     elif (('.' in self.label.text()) and button.text() == '0'):
-    #         newLabel = format(self.label.text() + button.text(),'.15')
-    #     else:
-    #         newLabel = format(str(self.label.text() + button.text()), '.15')
-
-    #     self.label.setText(newLabel)
+        self.outputLabel(newLabel)
 
 
 
+
+
+    
     def decimalClicked(self):
+        '''Введена точка'''
         if '.' not in self.label.text():
-            self.label.setText(self.label.text() + '.')
+            self.outputLabel(self.label.text() + '.')
 
 
 
     def unaryOperationsClicked(self):
+        '''Унарные операции'''
         button = self.sender()
-
         labelNumber = float(self.label.text())
-
         if button.text() == '+/-':
             labelNumber = labelNumber * -1
         else: # button text == '%'
             labelNumber = labelNumber * 0.01
+        self.outputLabel(labelNumber)
 
-        newLabel = format(labelNumber, '.15g')
-        self.label.setText(newLabel)
 
 
     def binaryOperationsClicked(self):
-        
+        '''Бинарные операции'''
         button = self.sender()
-
         self.firstNum = float(self.label.text())
-
         button.setChecked(True)
 
 
@@ -151,101 +130,83 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
 
         if self.pushButton_plus.isChecked():
             labelNumber = self.firstNum + secondNum
-            newLabel = format(labelNumber,'.15g')
-            self.label.setText(newLabel)
+            self.outputLabel(labelNumber)
             self.pushButton_plus.setChecked(False)
 
         elif self.pushButton_minus.isChecked():
             labelNumber = self.firstNum - secondNum
-            newLabel = format(labelNumber,'.15g')
-            self.label.setText(newLabel)
+            self.outputLabel(labelNumber)
             self.pushButton_minus.setChecked(False)
 
         elif self.pushButton_multiply.isChecked():
             labelNumber = self.firstNum * secondNum
-            newLabel = format(labelNumber,'.15g')
-            self.label.setText(newLabel)
+            self.outputLabel(labelNumber)
             self.pushButton_multiply.setChecked(False)
 
         elif self.pushButton_divide.isChecked():
             labelNumber = self.firstNum / secondNum
-            newLabel = format(labelNumber,'.15g')
-            self.label.setText(newLabel)
+            self.outputLabel(labelNumber)
             self.pushButton_divide.setChecked(False)
 
-        self.userIsTypingSecondNumber = False
+        self._userIsTypingSecondNumber = False
 
 
 
     def clearClicked(self):
+        '''Отчистка экрана'''
         self.pushButton_plus.setChecked(False)
         self.pushButton_minus.setChecked(False)
         self.pushButton_multiply.setChecked(False)
         self.pushButton_divide.setChecked(False)
-
-        self.userIsTypingSecondNumber = False
+        self._userIsTypingSecondNumber = False
         self.label.setText('0')
 
 
 
     def piClicked(self):
-
+        '''Число Пи'''
         self.label.setText(str(pi))
 
 
     def rdClicked(self):
-
-        self.label.setText(str(uniform(-1,1)))
+        '''Случайное число от -1 до 1'''
+        # self.label.setText(str(uniform(-1,1)))
+        self.label.setText(str(uniform(int(self.lineEdit_input1.text()),int(self.lineEdit_input2.text()))))
 
 
     def sqrtClicked(self):
+        '''Квадратный корень'''
         button = self.sender()
         labelNumber = self.label.text()
         if button.text() == 'sqrt' and float(labelNumber) > 0:
             labelNumber = sqrt(float(labelNumber))
-            newLabel = format(labelNumber, '.15g')
-            self.label.setText(newLabel)
+            self.outputLabel(labelNumber)
         else:
-            self.label.setText('Не определенно')
+            labelNumber = 'Не определенно'
+            self.outputLabel(labelNumber)
 
-        
+
 
 
     def numberSystemsClicked(self):
-
+        '''Перевод в СС'''
         button = self.sender()
-
         labelNumber = float(self.label.text())
-
         if button.text() == 'bin':
             labelNumber = bin(int(labelNumber))[2:]
-
         if button.text() == 'oct':
             labelNumber = oct(int(labelNumber))[2:]
-
         if button.text() == 'hex':
             labelNumber = hex(int(labelNumber))[2:]
-
-        newLabel = format(labelNumber, '.15s')
-        self.label.setText(newLabel)
-
-
-
-    # def binSystem(self):
-    #     button = self.sender()
-    #     labelNumber = self.label.text()
-        
-    #     if self.label.text() == 0 and self.pushButton_bin.down():
-    #         labelNumber = int(str(labelNumber), 2)
-    #     newLabel = format(labelNumber, '.15s')
-    #     self.label.setText(newLabel)
+        self.outputLabel(labelNumber)
         
 
     def convertGetResulst(self):
+        '''Конвертер в другие СС'''
         decimal_number = int(self.lineEdit_input1.text())
         base = int(self.lineEdit_input2.text())
         
-        STRINGS = '0123456789abcdef'
+        STRINGS = string.digits + string.ascii_lowercase 
         remainder_stack = []
 
         while decimal_number > 0:
@@ -257,22 +218,11 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         while remainder_stack:
             new_digits.append(STRINGS[remainder_stack.pop()])
 
-        result = ''.join(new_digits)
+        labelNumber = ''.join(new_digits)
         
+        self.outputLabel(labelNumber)
 
-        self.label.setText(result)
-
-           
-
-
-
-
-
-
-        # number = self.lineEdit_input1.text()
-        # base = int(self.lineEdit_input2.text())
-        # result = int(number, base)
-        # self.label.setText(str(result))
+        
 
 
 
