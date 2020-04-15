cbbca73b380ec5b9dacd3b2a1a0ae20aa508bdd7
Vad081 <60114907+Vad081@users.noreply.github.com>
2020-04-15 16:32:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication 
-from Calculator import CalculatorWindow
+from Gui.Calculator import CalculatorWindow
+from Gui.weather import WeatherWidget
 
 
 app = QApplication(sys.argv)
 
 calculator = CalculatorWindow()
+# weather = WeatherWidget()
+
 
 sys.exit(app.exec_())
